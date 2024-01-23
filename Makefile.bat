087e81ff709653f9b4deea962083a955fe02bb8f
DavidPlnmr <david.paulino@etu.hesge.ch>
2024-01-23 13:06:49 +0100

    Added Makefile.bat for windows

---
+++ Makefile.bat
@@ -0,0 +1,25 @@
+@echo off
+set LATEX_COMPILER=pdflatex
+set PYTHONTEX=pythontex
+set FILE_NAME=main
+set MAIN_TEX_FILE=%FILE_NAME%.tex
+
+rem Cible par défaut
+:all
+call :pdf
+goto :eof
+
+rem Règle pour générer le PDF
+:pdf
+%LATEX_COMPILER% %MAIN_TEX_FILE%
+%PYTHONTEX% %MAIN_TEX_FILE%
+%LATEX_COMPILER% %MAIN_TEX_FILE%
+rem Can be optional if you don't use biblatex
+%LATEX_COMPILER% %MAIN_TEX_FILE%
+goto :eof
+
+rem Règle pour nettoyer les fichiers générés
+:clean
+del *.aux *.log *.out *.pytxcode *.pytxmcr *.synctex.gz *.toc *.pdf *.fdb_latexmk *.fls 
+rmdir /s /q pythontex-files-%FILE_NAME%
+goto :eof
